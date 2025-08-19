c70aaca25ebe493c3bc602a8b198c0f574b4898f
Jessie0611 <imjustme0611@hotmail.com>
2025-08-19 14:24:41 -0500

    build to push changes into live version instead of test version

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<?php
+//RESOURCES === TESTING VERSION
+?>
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
+</head>
+
+<body>
+    <h1>Home</h1>
+    <p> .blade is a template that allows for you to create shortcuts</p>
+
+</body>
+
+</html>

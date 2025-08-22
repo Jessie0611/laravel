48fae71402d8bc224f8a04f050392d4c48773401
Jessie0611 <imjustme0611@hotmail.com>
2025-08-22 16:45:56 -0500

    grouped routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,40 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// ^we want to grab a certain class call routes
 
+//6 Different Types Of Routes: POST, GET, PUT, DELETE, patch, options
+
+//Get rout example
 Route::get('/', function () {
     return view('home');
 });
 
 Route::get('/contact', function () {
     return view('contact');
+});
+
+//Parameters using routes
+Route::get('/portfolio/{firstName}/{lastName}', function ($firstName, $lastName) {
+    return $firstName . " " . $lastName;
+});
+
+//named routes
+Route::get('/test', function () {
+    return "this is a test";
+});
+
+//grouped routes
+
+
+Route::get('/portfolio', function () {
+    return view('portfolio');
+});
+
+Route::get('/portfolio/company', function () {
+    return view('company');
+});
+
+Route::get('/portfolio/organization', function () {
+    return view('organization');
 });
